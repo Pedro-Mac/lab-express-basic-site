a601b82f54e1417b679fc00642cc6615fe163d87
KozaX <55134615+KozaX@users.noreply.github.com>
2020-07-20 19:01:04 +0200

    done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+app.get('/works', (req, res) => {
+  res.sendFile(__dirname + '/views/works.html');
+});
+app.get('/gallery', (req, res) => {
+  res.sendFile(__dirname + '/views/gallery.html');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html');
+});
+
+app.listen(3000);
